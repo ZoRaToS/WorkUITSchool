97ddeb2874c3ab79efc24a94165d2cd7f1681e78
DELWAIDR <dovakin.izotov1994@gmail.com>
2018-08-20 19:22:42 +0300

    добавлены конструкторы

--- src/com/company/oop/Phone.java
+++ src/com/company/oop/Phone.java
@@ -5,6 +5,14 @@ public class Phone {
     String model;
     double weight;
 
+    Phone(String number, String model, double weight) {
+        System.out.println("Конструктор с тремя параметрами");
+    }
+
+    Phone(String number, String model) {
+        System.out.println("Конструктор с двумя параметрами");
+    }
+
     public void receiveCall(String name) {
         System.out.println("Звонит " + name);
     }
@@ -12,5 +20,6 @@ public class Phone {
     public String getNumber() {
         return number;
     }
+
 
 }
--- src/com/company/oop/PhoneMain.java
+++ src/com/company/oop/PhoneMain.java
@@ -2,7 +2,8 @@ package com.company.oop;
 
 public class PhoneMain {
     public static void main(String[] args) {
-        Phone phone1 = new Phone();
+        Phone phone1 = new Phone("0958127244","Xiaomi");
+        new Phone("0958127244","Xiaomi",1);
         phone1.model = "LG";
         phone1.number = "0958127244";
         phone1.weight = 2.5;
